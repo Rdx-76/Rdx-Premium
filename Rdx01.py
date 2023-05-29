a87560199765e98c331c7a8f6a2ccde2245e5548
Rdx <132105892+Rdx-76@users.noreply.github.com>
2023-05-29 13:43:46 +0600

    Delete Rdx01.py

--- Rdx01.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-Rdx=platform.architecture()[0]
-if Rdx=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif Rdx=="64bit":
-    #print('Command is in update wait we will fix it soon !')
-    __import__("Rdx-Premium")
